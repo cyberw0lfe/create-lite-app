64a1b206c88272fd606891b7737474c0361f6515
Adam Wolfe <adamtwolfe@gmail.com>
2020-03-10 21:44:26 -0400

    initial commit

---
+++ src/cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const npm = require('npm')
+const { execSync, exec, spawnSync, spawn } = require('child_process')
+
+const execCmd = cmd => { // add args here too
+  // const child = spawnSync(cmd, [], { encoding: 'utf8' })
+  try {
+    const child = execSync(cmd, [], { encoding: 'utf8' })
+    if(child.error) console.log('ERROR: ', child.error)
+    return child
+  } catch(err) {
+    console.log('ERROR executing command: ', err)
+  }
+  
+}
+
+npm.load({
+  loaded: false
+}, async err => {
+  npm.commands.init(() => {
+    const { name } = require(`${process.cwd()}/package.json`)
+    execCmd(`mkdir ${name}`)
+    execCmd(`mv package.json ${name}`)
+  })
+})
